0c892e439a2fd0d34a1ecdfc4c1db70a66a1d877
pikazo <729262920@qq.com>
2019-10-18 16:28:28 +0800

    通过edm登录后,抓取edm分组邮箱信息

--- edmmailworm/edmmailworm/spiders/edmmailworm_spider.py
+++ edmmailworm/edmmailworm/spiders/edmmailworm_spider.py
@@ -1,17 +1,62 @@
+from urllib.request import Request
+
 import scrapy
+
+
+def authentication_failed(response):
+    # TODO: Check the contents of the response and return True if it failed
+    # or False if it succeeded.
+    if '","status":1,"' in response.text :
+        return True
+    else:
+        return False
+
+
+
 
 class EdmmailwormSpider(scrapy.spiders.Spider):
     name = "edm" #这个命令用于scrapy crawl edm 启动爬虫
-    allowed_domains = ["dmoz.org"]
+    # allowed_domains = ["dmoz.org"]
     start_urls = [
-        'http://quotes.toscrape.com/page/1/',
-        'http://quotes.toscrape.com/page/2/',
+        'https://www.bossedm.com/admin',
     ]
+    edmpageno = 0;
+
 
     def parse(self, response):
-        for quote in response.css('div.quote'):
-            yield {
-                'text': quote.css('span.text::text').get(),
-                'author': quote.css('small.author::text').get(),
-                'tags': quote.css('div.tags a.tag::text').getall(),
-            }
+        return scrapy.FormRequest.from_response(
+            response,
+             formdata={'username': 'username', 'pwd': 'pwd'},
+            callback=self.after_login
+        )
+
+    def after_login(self, response):
+        if authentication_failed(response):
+            self.logger.info("Login success")
+            return self.runningwor(response)
+        else:
+            self.logger.info("Login failed")
+            return
+
+    def runningwor(self,response):
+        smgui = response.url.split("/")
+        sid = smgui[-1] #获取到sid
+        self.logger.info("Login success");
+        while self.edmpageno < 1000 :
+            self.edmpageno= self.edmpageno + 1
+            # self.logger.info(sid);
+            # self.logger.info(str(self.edmpageno));
+            yield  scrapy.Request(url="https://www.bossedm.com/Admin/Subscriber/groupMembers/list_id/19/pageno/"+ str(self.edmpageno) +"/sid/"+sid, callback=self.parseMailaddr)
+
+
+    def parseMailaddr(self,response):
+        mailboxs = response.xpath("//tr//td[2]/a/text()")
+        for mailbox in mailboxs :
+            # self.logger.info(response.body)
+            # self.logger.info(mailbox.get())
+            yield {"mailbox":mailbox.get()}
+
+
+
+
+
---
+++ edmmailworm/test.py
@@ -0,0 +1,6 @@
+
+
+
+smgui = 1+1
+xiami = "1sdfsdfsfsdfhahaha"+str(smgui)+"sjlkjlsa"
+print(xiami)
